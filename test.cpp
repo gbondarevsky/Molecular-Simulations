06a62a2dfb10370f0bcbf124ac3f1bed5cf88d80
gbondarevsky <bonda005@umn.edu>
2014-02-02 22:02:20 -0600

    Minimum test program
    The C compiler wasn’t working well for me at all and I have no idea
    why. Here is a C++ test program that I compiled using g++ which worked
    much better.

---
+++ test.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#define _USE_MATH_DEFINES
+#include <cmath>
+
+using namespace std;
+
+int main(){
+        cout << "Hello World\n";
+        cout << M_PI << "\n";
+        return 0;
+}
