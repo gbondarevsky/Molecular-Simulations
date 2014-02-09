aecbf86b92a986bb662feed89624b4a933308bda
Gary Bondarevsky <bonda005@umn.edu>
2014-02-09 17:26:59 -0600

    no message

--- MD_main.cpp
+++ MD_main.cpp
@@ -3,40 +3,43 @@
 #define _USE_MATH_DEFINES
 #include <cmath>
 #include <ctime>
+#include <vector>
 
 using namespace std;
+using std::vector;
 
 //Global Variables
+float coords[216][3];
+//float **Coords = coords[0][0];
 
 //Function Prototypes
-float genCoords();
-void printarray(float arr[][3], int rows, int columns);
+int genCoords();
 
 
 int main(){
-        printarray(genCoords(),216,3);
-        return 0;
-}
-
-float genCoords(){
-        float coords [216][3]; //Rows are each particle. Columns are x, y, z, coords
-        for (int i = 0; i <= 215; i++){
-                coords[i][0] = float(i%18)*1.75; //Fill all x coords 
-        }
-        for (int i = 0; i <= 215; i++){
-                coords[i][1] = float(i%16)*1.75; //Fill y coords
-        }
-        for (int i = 0; i <= 215; i++){
-                coords[i][2] = float(i%6)*1.75; //Fill z coords
-        }
-        return coords[215][2];
-}
-
-void printarray(float arr[][3],int rows,int columns){
-        for (int j = 0; j < columns; j++){
-                for (int i = 0; i < rows; i++){
-                        cout << arr[i][j] << "\t";
+	genCoords();
+	cout << 216 << "\n";
+	cout << "#" << "\n";
+	for (int j = 0; j < 216; j++){
+				cout << "Ar ";
+                for (int i = 0; i < 3; i++){
+                        cout << coords[j][i] << " ";
                 }
                 cout << "\n";
         }
+        return 0;
+}
+
+int genCoords(){
+    for (int i = 0; i <= 215; i++){
+        coords[i][0] = (float(i%18)*1.75) + (float(i/72)*1.75); //Fill all x coords
+    }
+    for (int i = 0; i <= 215; i++){
+    	if (i%2 == 0){coords[i][1] = (float(2*((i%72)/18))*3.5) + (float(i/72)*1.75);}
+    	else {coords[i][1] = (float(2*((i%72)/18)+1)*3.5) + (float(i/72)*1.75);}
+    }
+    for (int i = 0; i <= 215; i++){
+		coords[i][2] = float(i/72)*3.5;
+    }
+        return 0;
 }
